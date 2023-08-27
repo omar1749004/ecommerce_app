19522918d93b2e4a75dd233de35ceff11b3ffbc0
omar1749004 <mhsnmr68@gmail.com>
2023-08-27 20:51:03 +0300

    add date for order and refresh order page

--- lib/core/functions/fcmconfig.dart
+++ lib/core/functions/fcmconfig.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/controller/orders/pending_controller.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:get/get.dart';
 import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
@@ -19,5 +20,13 @@ fcmconfig(){
   FirebaseMessaging.onMessage.listen((message) {
     FlutterRingtonePlayer.playNotification();
     Get.snackbar(message.notification!.title!, message.notification!.body!);
+    refreshNotification(message.data);
   });
+}
+refreshNotification(data){
+  if(Get.currentRoute == "/ordersPending" && data["pagename"] =="refreshOrderPending")
+  {
+    PendingOrdersControllerImp controller = Get.find();
+    controller.refreshOrder();
+  }
 }
---
+++ lib/views/widget/order/order_card.dart
@@ -0,0 +1,53 @@
+import 'package:e_commerce_app/controller/orders/pending_controller.dart';
+import 'package:e_commerce_app/core/constant/color.dart';
+import 'package:e_commerce_app/data/models/orders_model.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/simple/get_view.dart';
+import 'package:jiffy/jiffy.dart';
+
+class CardOrders extends GetView<PendingOrdersControllerImp> {
+  const CardOrders({super.key, required this.ordersModels});
+  final OrdersModels ordersModels;
+  @override
+  Widget build(BuildContext context) {
+    return  Card(child: Container(
+        padding:const EdgeInsets.all(10),
+        
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [ 
+          Row(children: [
+             Text("Order Number : #${ordersModels.ordersId}", style:const TextStyle(
+          fontSize: 18, fontWeight: FontWeight.bold),),
+          Spacer(),
+         // Text(ordersModels.ordersDate)
+         Text(Jiffy.parse(
+          ordersModels.ordersDate , pattern: 'yyyy-MM-dd HH:mm:ss').fromNow(),
+          style:const TextStyle(color: ColorApp.KPrimaryColor,fontWeight: FontWeight.bold),
+         )
+          ],),
+         const Divider(color: Colors.black,),
+          Text("Order Type : ${controller.printOrderType(ordersModels.ordersType)}"),
+          Text("Order Price : ${ordersModels.ordersPrice} \$",style: TextStyle(height: 2),),
+          Text("Delivery Price : ${ordersModels.ordersPricedelivery} \$",style: TextStyle(height: 2)),
+          Text("Payment Methode : ${controller.printOrderPaymentMethod(ordersModels.ordersPaymentmethod)}",
+          style:const TextStyle(height: 2)),
+          Text("Order Status : ${controller.printOrderStatuse(ordersModels.ordersStatuse)} ",style: TextStyle(height: 2)),
+        const Divider(color: Colors.black,),
+          Row(
+            children: [
+              Text("Total Price : ${ordersModels.ordersTotalprice} \$" ,style:const TextStyle(
+              color: ColorApp.KPrimaryColor, fontWeight: FontWeight.bold),),
+             const Spacer(),
+              MaterialButton(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                onPressed: (){}, 
+                color: const Color.fromARGB(255, 195, 159, 250),
+               child:const Text("Details",style: TextStyle(color: ColorApp.secondColor),),
+               
+              )
+            ],
+          )
+       ]),),);
+  }
+}
